2ea10f7243168f0743ac4484d5703c5999768278
Artur Avetisyan <artaveti@c2r15s3.42yerevan.am>
2023-12-15 13:47:12 +0400

    15.12.2023

---
+++ DaysInAMonth.cs
@@ -0,0 +1,41 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+        int month = 1;
+        int year = 2023;
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+        {
+            Console.WriteLine(31);
+        }
+        else if (month == 4 || month == 6 || month == 9 || month == 11)
+        {
+            Console.WriteLine(30);
+        }
+        else if (month == 2)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine(29);
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine(28);
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine(29);
+            }
+            else
+            {
+                Console.WriteLine(28);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid month.");
+        }
+	}
+}
